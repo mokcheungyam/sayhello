6f0379f1a5303e4540debf12fa7bd6282a2aa0e3
mokcheungyam <609356067@qq.com>
2021-01-09 09:46:48 +0800

    [update] html

--- sayhello/settings.py
+++ sayhello/settings.py
@@ -4,7 +4,6 @@ import sys
 from sayhello import app
 
 dev_db = "sqlite:///" + os.path.join(os.path.dirname(app.root_path), "data/data.db")
-print(dev_db)
 
 SECRET_KEY = os.getenv("SECRET_KEY", "secret string")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
